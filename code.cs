0e43bd4fbdabeca94bcbf8d35dfcc5c336414e9c
CrimsonUnited <103459068+CrimsonUnited@users.noreply.github.com>
2022-06-07 11:12:31 -0700

    Update code.cs

--- code.cs
+++ code.cs
@@ -1,7 +1,7 @@
 using System;
 using f2d;
 
-namespace f2d {
+public namespace f2d {
  public class Converter {
      public double Convert (int numerator, int denominator) {
          return (double)numerator / (double)denominator;
