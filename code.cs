3378962d989dcdcc7e827d4ee2dcf442ad69cc59
CrimsonUnited <103459068+CrimsonUnited@users.noreply.github.com>
2022-06-07 11:10:49 -0700

    Create code.cs

---
+++ code.cs
@@ -0,0 +1,10 @@
+using System;
+using f2d;
+
+namespace f2d {
+ public class Converter {
+     public double Convert (int numerator, int denominator) {
+         return (double)numerator / (double)denominator;
+     }
+ }
+}
